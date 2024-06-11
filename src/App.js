c845c1e65e63123d3b8c29c92c9ff4bb616ff4a9
kennzuki <kingsyken@gmail.com>
2024-06-12 00:24:20 +0300

    setup ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useRef } from "react";
+import { FaTrash } from 'react-icons/fa';
 
 function App() {
+
+  const [todos, setTodos] = useState([])
+  const inputRef=useRef()
+  const add= () => {
+     const inputText=inputRef.current.value.trim()
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='text-center flex flex-col gap-8 items-center place-items-center p-12 border'>
+      <h1 className='text-3xl font-bold  text-blue-600'>ken todo list</h1>
+      <section className='flex gap-2 place-items-center border-b py-4'>
+        <input
+          type='text'
+          className='border p-2 rounded-xl'
+          placeholder='Add todo'
+          ref={inputRef}
+        />
+        <button onClick={add} className='px-3 py-1 bg-yellow-500 text-white font-bold rounded'>
+          Add+
+        </button>
+      </section>
+      <section className=''>
+        <h1 className='uppercase flex gap-2 place-items-center'>
+          joined a club{' '}
+          <span className='text-red-500'>
+            <FaTrash />
+          </span>
+          <span className='bg-blue-500 rounded-xl py-1 px-3 text-white font-bold'>Edit</span>
+        </h1>
+      </section>
     </div>
   );
 }
