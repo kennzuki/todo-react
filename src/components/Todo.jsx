696d63e9b702e9fc17217a672f5883f21e92c3eb
kennzuki <kingsyken@gmail.com>
2024-06-12 15:51:09 +0300

    add function and inputref to get value from input

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,6 +2,20 @@ import TodoItems from './TodoItems';
 import { useState, useRef } from 'react';
 
 const Todo = () => {
+  const [todos, setTodos] = useState([])
+  const inputRef = useRef()
+  const add = () => {
+    const inputText = inputRef.current.value.trim()
+    console.log(inputText);
+    const newTodos={
+      id: Date.now(),
+      text: inputText,
+      isCompleted:false  
+    }
+    setTodos((prev)=>[...prev,newTodos])
+  
+  }
+
   return (
     <div className=''>
       <section className='flex gap-2 place-items-center border-b py-4'>
@@ -9,8 +23,9 @@ const Todo = () => {
           type='text'
           className='border p-2 rounded-xl'
           placeholder='Add todo'
+          ref={inputRef}
         />
-        <button className='px-3 py-1 bg-yellow-500 text-white font-bold rounded'>
+        <button onClick={add} className='px-3 py-1 bg-yellow-500 text-white font-bold rounded'>
           Add+
         </button>
       </section>
