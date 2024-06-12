2fda76514bdc1cd570eca10238a7d2028ba3af2c
kennzuki <kingsyken@gmail.com>
2024-06-12 16:02:19 +0300

    map and returned todo in todoitems

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,19 +2,18 @@ import TodoItems from './TodoItems';
 import { useState, useRef } from 'react';
 
 const Todo = () => {
-  const [todos, setTodos] = useState([])
-  const inputRef = useRef()
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef();
   const add = () => {
-    const inputText = inputRef.current.value.trim()
+    const inputText = inputRef.current.value.trim();
     console.log(inputText);
-    const newTodos={
+    const newTodos = {
       id: Date.now(),
       text: inputText,
-      isCompleted:false  
-    }
-    setTodos((prev)=>[...prev,newTodos])
-  
-  }
+      isCompleted: false,
+    };
+    setTodos((prev) => [...prev, newTodos]);
+  };
 
   return (
     <div className=''>
@@ -25,16 +24,19 @@ const Todo = () => {
           placeholder='Add todo'
           ref={inputRef}
         />
-        <button onClick={add} className='px-3 py-1 bg-yellow-500 text-white font-bold rounded'>
+        <button
+          onClick={add}
+          className='px-3 py-1 bg-yellow-500 text-white font-bold rounded'
+        >
           Add+
         </button>
       </section>
       {/* todo list */}
-      <section className='flex flex-col gap-2'>
-        <ul className='uppercase flex flex-col gap-2 items-center'></ul>
+      <section className=''>
+        {todos.map((items, index) => {
+          return <TodoItems key={index} text={items.text} />;
+        })}
       </section>
-      <TodoItems text='learning TS' />
-      <TodoItems text='Learning next js' />
     </div>
   );
 };
